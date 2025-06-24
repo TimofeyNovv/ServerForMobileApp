5ee969d9f4d1ee3f4d177b8c3523806f136b054a
TimofeyNov <timn2020@inbox.ru>
2025-06-24 13:06:20 +0300

    change localhost to ip adress

--- src/main/java/com/example/mobileAppServer/domain/service/UserService.java
+++ src/main/java/com/example/mobileAppServer/domain/service/UserService.java
@@ -6,5 +6,7 @@ import com.example.mobileAppServer.domain.entity.UserEntity;
 public interface UserService {
     public UserEntity findByEmail(String email);
 
+    public UserEntity findById(Integer id);
+
     public UserEntity create(UserEntity userEntity);
 }
